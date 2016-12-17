f2fe831095c9a1dee4ff481c93a9707597469afb
Tom Jardine-McNamara <tom@itsravenous.com>
2016-12-17 12:27:24 +0000

    Add claim frontend

---
+++ time.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('load')
+  document.forms[0].addEventListener('submit', function(e) {
+    e.preventDefault();
+    return false;
+  });
+  document.forms[0].addEventListener('click', function(e) {
+    var el = e.target || e.srcElement;
+    if(el.nodeName.toLowerCase() === 'button') {
+      claim(el.name, el.value);
+    }
+  });
+});
+
+function claim() {
+  var url = document.forms[0].action;
+  var request = new XMLHttpRequest();
+  request.open('POST', url, true);
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      this.response;
+    } else {
+      // We reached our target server, but it returned an error
+
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+
+  request.send();
+}
