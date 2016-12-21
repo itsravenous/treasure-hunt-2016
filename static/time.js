2b7b9416e830abc918c6c6c804b1c9ed6614e3b5
Alan Jenkins <alan.christopher.jenkins@gmail.com>
2016-12-21 11:33:29 +0000

    frontend: show detected errors - AJAX, javascript (e.g. JSON parse exception)

--- static/time.js
+++ static/time.js
@@ -1,3 +1,21 @@
+// TODO
+function showError(message) {
+  document.body.textContent = message;
+}
+function unexpectedError() {
+  showError('Sorry, there was an error in this application.');
+}
+function networkError() {
+  showError('Failed to connect to server.  Check your network connection.  Are you using the right WiFi network?');
+}
+
+window.addEventListener('error', function() {
+  // Javascript exception (or maybe javascript syntax error)
+  unexpectedError();
+});
+// We have no detection of asset load errors
+
+
 document.addEventListener('DOMContentLoaded', function() {
   console.log('load')
   document.forms[0].addEventListener('submit', function(e) {
@@ -19,15 +37,24 @@ function claim(name, value) {
   request.onload = function() {
     if (this.status >= 200 && this.status < 400) {
       // Success!
-      this.response;
+      result = JSON.parse(this.response);
+
+      // TODO
+      showError(JSON.stringify(result));
     } else {
       // We reached our target server, but it returned an error
 
+      // TODO
+      // This was under-specified.
+      // It should have been JSON, with a specific error code.
+      // It's OK to treat JSON as text, for right now though.
+      showError(this.response);
     }
   };
 
   request.onerror = function() {
     // There was a connection error of some sort
+    networkError();
   };
 
   request.send();
